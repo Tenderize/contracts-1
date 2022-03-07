2a5cd4a78ddc42ee7909b5dbd9fdd17417bd8c43
Nico Vergauwen <vergauwennico@gmail.com>
2022-03-07 12:23:13 +0100

    scripts

---
+++ scripts/rewards.js
@@ -0,0 +1,112 @@
+const contracts = require('../contractAddresses.json')
+
+const ethUtils = require('ethereumjs-util')
+
+const getBlockHeader = require('../test/helpers/blocks').getBlockHeader
+const MerkleTree = require('./utils/merkle-tree')
+
+const utils = require('./utils/utils')
+
+const StakeManager = artifacts.require('StakeManager')
+const RootChain = artifacts.require('RootChain')
+const ValidatorShare = artifacts.require('ValidatorShare')
+const ethers = require('ethers')
+
+const validatorAccount = process.argv[6]
+
+const privKey = process.env.PRIV_KEY
+
+async function getStakeManager() {
+  return StakeManager.at(contracts.root.StakeManagerProxy)
+}
+
+let offset = 1
+let lastEndBlock = 0
+
+async function submitHeaderBlock() {
+  const childWeb3 = new ethers.providers.JsonRpcProvider('http://144.126.216.112:8545')
+  const wallet = new ethers.Wallet(privKey)
+  const proposer = wallet
+  const event = {
+    block: await childWeb3.getBlock(
+      'latest',
+      true /* returnTransactionObjects */
+    )
+  }
+
+  // rootChain expects the first checkpoint to start from block 0.
+  // However, ganache would already be running and would be much ahead of block 0.
+  // offset is used to treat the block of the first checkpoint to be 0
+  if (offset == null) {
+    offset = event.block.number
+  }
+  event.block.number -= offset // rootChain will thank you for this
+  // Submit only first 100 blocks
+  event.block.number = event.block.number > 100 ? 100 : event.block.number
+  const start = lastEndBlock + 1
+  const end = event.block.number
+  lastEndBlock = end
+  if (start > end) {
+    throw new Error(`Invalid end block number for checkpoint`, { start, end })
+  }
+
+  const headers = []
+  for (let i = start; i <= end; i++) {
+    const block = await childWeb3.getBlock(i + offset)
+    block.number = i
+    headers.push(getBlockHeader(block))
+  }
+
+  const blockHeader = getBlockHeader(event.block)
+  const tree = new MerkleTree(headers)
+  const root = ethUtils.bufferToHex(tree.getRoot())
+  // tree
+  //   .verify(blockHeader, end - start, tree.getRoot(), blockProof)
+  //   .should.equal(true)
+  const { data, sigs } = utils.buildsubmitCheckpointPaylod(
+    proposer.address,
+    start,
+    end,
+    root,
+    [proposer],
+    { getSigs: true, allValidators: true }
+  )
+
+  const rootChain = await RootChain.at(contracts.root.RootChainProxy)
+  const res = await rootChain.submitCheckpoint(
+    data, sigs)
+  return res
+}
+
+async function checkStake() {
+  const stakeManager = await getStakeManager()
+  const validatorID = await stakeManager.signerToValidator(validatorAccount)
+  const validator = await stakeManager.validators(validatorID)
+  console.log('val share', validator.contractAddress)
+  const valShare = await ValidatorShare.at(validator.contractAddress)
+
+  console.log('stakeManager.totalRewards:', web3.utils.fromWei(await stakeManager.totalRewards()))
+  console.log('valShare.getRewardPerShare:', web3.utils.fromWei(await valShare.getRewardPerShare()))
+  // console.log('valShare.rewards:', web3.utils.fromWei(await valShare.rewards()))
+
+  // const accounts = await web3.eth.getAccounts()
+
+  // const exchangeRate = await valShare.exchangeRate()
+  // const shares = (await valShare.balanceOf(accounts[1]))
+  // const prec = web3.utils.toBN(100)
+  // console.log('share', shares.toString())
+  // console.log('fx rate', exchangeRate.toString())
+  // const tokensFromShares = exchangeRate.mul(shares).div(prec)
+  // console.log(web3.utils.fromWei(tokensFromShares))
+}
+
+module.exports = async function(callback) {
+  try {
+    await submitHeaderBlock()
+    await checkStake()
+  } catch (e) {
+    // truffle exec <script> doesn't throw errors, so handling it in a verbose manner here
+    console.log(e)
+  }
+  callback()
+}
---
+++ scripts/setup.js
@@ -0,0 +1,47 @@
+const contracts = require('../contractAddresses.json')
+
+const utils = require('./utils/utils')
+
+const RootToken = artifacts.require('TestToken')
+const StakeManager = artifacts.require('StakeManager')
+
+async function getStakeManager() {
+  return StakeManager.at(contracts.root.StakeManagerProxy)
+}
+
+async function stake() {
+  console.log(process.argv)
+  const validatorAccount = process.argv[6]
+  // pubkey should not have the leading 04 prefix
+  const pubkey = process.argv[7]
+  const stakeAmount = web3.utils.toWei(process.argv[8] || '10000')
+  const heimdallFee = web3.utils.toWei(process.argv[9] || '1')
+  console.log(`Staking ${stakeAmount} for ${validatorAccount}...`)
+
+  const stakeManager = await getStakeManager()
+  const maticToken = await RootToken.at(contracts.root.tokens.MaticToken)
+  console.log({ stakeManager: stakeManager.address, maticToken: maticToken.address, stakeToken: await stakeManager.token() })
+  console.log('Sender accounts has a balanceOf', (await maticToken.balanceOf(validatorAccount)).toString())
+  await maticToken.approve(stakeManager.address, web3.utils.toWei('1000000'), { from: validatorAccount })
+  await delay(5)
+  console.log('sent approve tx, staking now...')
+  console.log(await stakeManager.validators(await stakeManager.getValidatorId(validatorAccount)))
+  // Remember to change the 4th parameter to false if delegation is not required
+  await stakeManager.stakeFor(validatorAccount, stakeAmount, heimdallFee, true, pubkey, { from: validatorAccount })
+  
+  return delay(5)
+}
+
+module.exports = async function(callback) {
+  try {
+    await stake()
+  } catch (e) {
+    // truffle exec <script> doesn't throw errors, so handling it in a verbose manner here
+    console.log(e)
+  }
+  callback()
+}
+
+function delay(s) {
+  return new Promise(resolve => setTimeout(resolve, s * 1000))
+}
